4dedb4cb41d38c1eefa3baac35941174971f4ad7
Yuanshu Cai <caiyuanshu@gmail.com>
2024-09-17 17:08:45 -0400

    adding new component needs to style

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import "./App.scss";
 import CustomCursor from "./components/CustomCursor/CustomCursor";
 import Header from "./components/Header/Header";
 import TextDisplay from "./components/TextDisplay/TextDisplay";
-
+import CardsDisplay from "./components/CardsDisplay/CardsDisplay";
 function App() {
   return (
     <>
       <CustomCursor />
       <Header />
+      <CardsDisplay />
       <TextDisplay />
     </>
   );
