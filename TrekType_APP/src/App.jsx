a9863274901d4232696a48795c8afa066ae1c070
Yuan Cai <caiyuanshu@gmail.com>
2024-09-25 10:19:59 -0400

    adding a warp background

--- TrekType_APP/src/App.jsx
+++ TrekType_APP/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.scss";
 import CustomCursor from "./components/CustomCursor/CustomCursor";
+import WarpBackground from "./components/WarpBackground/WarpBackground";
 import Header from "./components/Header/Header";
 import MBTIPage from "./pages/MBTIPage/MBTIPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -8,15 +9,18 @@ import ResultPage from "./pages/ResultPage/ResultPage";
 function App() {
   return (
     <>
-      <Router>
-        <CustomCursor />
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/mbti/:id" element={<MBTIPage />} />
-          <Route path="/result" element={<ResultPage />} />
-        </Routes>
-      </Router>
+      <WarpBackground />
+      <div className="content-wrapper">
+        <Router>
+          <CustomCursor />
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/mbti/:id" element={<MBTIPage />} />
+            <Route path="/result" element={<ResultPage />} />
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
